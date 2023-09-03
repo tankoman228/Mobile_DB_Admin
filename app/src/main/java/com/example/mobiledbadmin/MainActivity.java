6bb0fe7d7fa68cc06ee1df37ecdfa1665328c4c8
Tank 228 <92095847+tankoman228@users.noreply.github.com>
2023-09-03 19:56:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mobiledbadmin/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.mobiledbadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, table_create.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/mobiledbadmin/table_create.java
@@ -0,0 +1,15 @@
+package com.example.mobiledbadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class table_create extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_create);
+    }
+}
